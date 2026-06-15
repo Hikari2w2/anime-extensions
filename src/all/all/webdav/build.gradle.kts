5b15a6abe336ba5e32b8e647dc9a0e1a81943803
*namirin's husband <124070334+Hikari2w2@users.noreply.github.com>
2026-06-15 18:02:56 -0400

    Add WebDAV extension configuration to build.gradle.kts

--- src/all/all/webdav/build.gradle.kts
+++ src/all/all/webdav/build.gradle.kts
@@ -1,1 +1,14 @@
+plugins {
+    id("com.aniyomi.extension")
+}
+apply(plugin = "com.aniyomi.extension")
 
+ext {
+    extName = "WebDAV"
+    extClass = ".WebDAV"
+    extVersionCode = 1
+}
+
+dependencies {
+    implementation("com.github.thegrizzlylabs:sardine-android:0.9")
+}
